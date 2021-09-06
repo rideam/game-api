874a30d5e99dad99eb4636a66f253534b9c7a6ec
Tatenda <tatendamvh@gmail.com>
2021-09-06 18:02:58 +0200

    added get route for games

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var gamesRouter = require('./routes/games');
+
 
 var app = express();
 
@@ -16,5 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/games', [ gamesRouter ]); 
+
 
 module.exports = app;
