49985623aeb6b5e13c39d395b0423f6f22815480
Tatenda <tatendamvh@gmail.com>
2021-09-06 19:50:49 +0200

    add basic auth

--- app.js
+++ app.js
@@ -2,9 +2,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const passport = require('passport');  
+const BasicStrategy = require('passport-http').BasicStrategy; 
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+passport.use(new BasicStrategy(
+    function(username, password, done) {
+      if (username === process.env.USERNAME && password === process.env.PASSWORD){
+        return done(null, {username: process.env.USERNAME }); 
+      }
+      else {
+        return done(null, false); 
+      }
+    })
+  );
+  
+
 var gamesRouter = require('./routes/games');
 
 
@@ -18,7 +34,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/games', [ gamesRouter ]); 
+// app.use('/games', [ gamesRouter ]); 
+app.use('/games', [passport.authenticate('basic', {session: false}), gamesRouter]); 
 
 
 module.exports = app;
